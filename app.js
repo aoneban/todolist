5ba593d924e8b24fca4f180f19585cb19127663e
aoneban <torgregion1@gmail.com>
2024-05-17 23:15:44 +0200

    feat: add save tasks to the localstorage

--- app.js
+++ app.js
@@ -1,74 +1,95 @@
-//creating a delete button
-const createNodeList = document.getElementsByTagName('li');
+const generateStartTask = () => {
+  if (localStorage.length === 0) {
+    const ulList = document.getElementById('ulList');
+    for (let i = 0; i < 5; i += 1) {
+      const task = document.createElement('li');
+      task.classList.add('list-tasks');
+      let span = document.createElement('span');
+      let text = document.createTextNode('\u00D7');
+      span.className = 'close';
+      span.addEventListener('click', removeTask);
+      span.appendChild(text);
+      task.textContent = `Task number ${i + 1}`;
+      if (i % 2 !== 0) {
+        task.classList.add('oneTask');
+      }
+      task.append(span);
+      ulList.append(task);
+    }
+  } else {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const ulList = document.getElementById('ulList');
+    tasks.forEach((task) => {
+      const li = document.createElement('li');
+      li.textContent = task.text;
+      li.className = task.classes;
+      let span = document.createElement('span');
+      let text = document.createTextNode('\u00D7');
+      span.className = 'close';
+      span.addEventListener('click', removeTask);
+      span.appendChild(text);
+      li.append(span);
+      ulList.appendChild(li);
+    });
+  }
+};
+generateStartTask();
 
-for (let i = 0; i <= createNodeList.length - 1; i++) {
-  let span = document.createElement('span');
-  let text = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(text);
-  createNodeList[i].appendChild(span);
-}
-
-// create logic for deleting old tasks
-
-let close = document.querySelectorAll('.close');
-close.forEach((elem) => {
-  elem.addEventListener('click', (e) => {
-    let currentElement = e.target;
-    currentElement.parentNode.style.transform = 'translate(150%)';
-    setInterval(() => {
-      currentElement.parentNode.remove();
-    }, 500);
-  });
+const performTask = document.querySelector('ul');
+performTask.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('oneTask');
+    funcForAddToLocalStorage();
+  }
 });
 
-// make the task complete
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', createNewTask);
 
-let performdTask = document.querySelector('ul');
-performdTask.addEventListener('click', (e) => {
-  let currentElement = e.target;
-  currentElement.classList.toggle('oneTask');
-});
-
-// add a new task to the list
-
-function myFunction() {
-  let currentUl = document.getElementById('ul');
+function createNewTask() {
+  let currentUl = document.getElementById('ulList');
   let newData = document.getElementById('currInput').value;
 
   if (newData.length <= 4) {
     alert('Minimum 5 letters!');
-    return (document.getElementById('currInput').value = '');
+    document.getElementById('currInput').value = '';
+    return;
   }
-
   let newPost = document.createElement('li');
+  newPost.classList.add('list-tasks');
   newPost.innerText = newData;
   let span = document.createElement('span');
   let text = document.createTextNode('\u00D7');
   span.className = 'close';
+  span.addEventListener('click', removeTask);
   span.appendChild(text);
   newPost.appendChild(span);
   currentUl.insertAdjacentElement('beforeend', newPost);
   document.getElementById('currInput').value = '';
 
-  // delete all tasks along with newly created ones
-
-  let close__2 = document.querySelectorAll('.close');
-  close__2.forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-      let currentElement = e.target;
-      currentElement.parentNode.style.transform = 'translate(150%)';
-      setInterval(() => {
-        currentElement.parentNode.remove();
-      }, 500);
-    });
-  });
+  funcForAddToLocalStorage();
 }
-
-// create the ability to add tasks via the enter key
 
 document.addEventListener('keyup', (event) => {
   if (event.code === 'Enter' && event.target.nodeName == 'INPUT') {
-    myFunction();
+    createNewTask();
   }
 });
+
+function funcForAddToLocalStorage() {
+  const tasks = [];
+  const listItems = document.querySelectorAll('#ulList li');
+  listItems.forEach((item) => {
+    tasks.push({
+      text: item.childNodes[0].nodeValue.trim(),
+      classes: item.className,
+    });
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function removeTask(event) {
+  const item = event.target.parentNode;
+  item.remove();
+  funcForAddToLocalStorage();
+}
